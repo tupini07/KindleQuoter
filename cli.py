08c4f5bf85194b7b63a5114e97bf7d0b480bb191
Andrea Tupini <tupini07@gmail.com>
2021-08-05 08:28:38 -0600

    add ability to skip clippings

--- cli.py
+++ cli.py
@@ -1,4 +1,5 @@
 import click
+from colorama import Back, Fore
 
 import data
 import twitter
@@ -10,13 +11,19 @@ def cli():
 
 
 @cli.command()
-def post_single_quote():
+@click.option('--skip', is_flag=True, help="If specified, the next clipping will be marked as processed but won't actually be posted to twitter")
+def post_single_quote(skip):
     """
     Posts a single unprocessed quote to Twitter
     """
 
     unprocessed_clip = data.get_oldest_unprocessed_clipping()
-    twitter.post_tweet(unprocessed_clip)
+
+    if not skip:
+        twitter.post_tweet(unprocessed_clip)
+        print(Fore.GREEN + "Tweet posted!")
+    else: 
+        print(Fore.YELLOW + "Skipping tweet!")
+        print(Fore.LIGHTCYAN_EX + Back.MAGENTA + unprocessed_clip.body)
 
     data.mark_clipping_as_processed(unprocessed_clip)
-    print("Tweet posted!")
